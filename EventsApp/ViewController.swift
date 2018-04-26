2881804744a5a0323d3cd9b34b4d2c1654cbeab2
salimbraksa <salim.braksa@gmail.com>
2018-04-26 16:23:56 +0100

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Salim Braksa on 4/26/18.
+//  Copyright © 2018 Hidden Founders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
